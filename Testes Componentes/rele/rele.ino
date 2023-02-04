d76047767fad7ed57c307d1d2e525910a8c6d9c9
Fernando Dorneles <111237468+dornelesfernando@users.noreply.github.com>
2023-02-04 10:26:29 -0300

    Update rele.ino

--- Testes Componentes/rele/rele.ino
+++ Testes Componentes/rele/rele.ino
@@ -1,27 +1,45 @@
-# define rele 13  // Relé conectada ao pino 13 do arduino
-const int botao = 2;  // Botão conectado ao pino 2 do arduino
-int statusDoBotao = 0;  
-int led1 = 3, led2 = 4;  // Led que irão indicar se o motor está ligado ou desligado
+# define rele 13  // Relé conectada ao porta 13 do arduino
+
+// Botão liga/pausa
+const int botaoLiga = 11;  // Botão conectado a porta 11 do arduino
+int statusDoBotaoLiga = 0;  
+
+// Botão desliga/zera
+const int botaoDesliga = 12;  // Botão conectado a porta 12 do arduino
+int statusDoBotaoDesliga = 0;  
+
+int  led1 = 2, led2 = 5; // Led que irão indicar se o motor está ligado ou desligado
+
 
 void setup (){
     pinMode(led1, OUTPUT);
     pinMode(led2, OUTPUT);
     pinMode(rele, OUTPUT);
-    pinMode(botao, INPUT);
+    pinMode(botaoLiga, INPUT);
+    pinMode(botaoDesliga, INPUT);
+
+    Serial.begin(9600);
+    Serial.print("Teste Relé");
+    Serial.println("");
 }
 
 void loop (){
   
-    statusDoBotao = digitalRead(botao);  
-        if (statusDoBotao == HIGH){  // Relé ligada
+    statusDoBotaoLiga = digitalRead(botaoLiga);  
+        if (statusDoBotaoLiga == HIGH){  // Relé ligada
         digitalWrite(led1, LOW);
         digitalWrite(led2, HIGH);
         digitalWrite(rele, HIGH);
-        delay(50);
-        digitalWrite(rele, LOW);
+        Serial.println("Relé Ligada");
+        delay(150);
     }
-        else{  // Relé desligada
+
+    statusDoBotaoDesliga = digitalRead(botaoDesliga);  
+        if (statusDoBotaoDesliga == HIGH){  // Relé Desligada
         digitalWrite(led1, HIGH);
         digitalWrite(led2, LOW);
+        digitalWrite(rele, LOW);
+        Serial.println("Relé Desligada");
+        delay(150);
     }
 }
